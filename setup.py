1b2c8def2d6ed28f6bbfe64fb177e54da2b9fa74
Philipp Middendorf <middendorf@plapadoo.de>
2019-02-21 17:08:33 +0100

    Initial version of distutils integration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+steamworks_module = Extension("SteamworksPy", sources=["SteamworksPy.cpp"])
+
+setup(
+    name="SteamworksPy",
+    version="1.41",
+    ext_modules=[steamworks_module],
+    py_modules=["SteamworksPy"],
+)
